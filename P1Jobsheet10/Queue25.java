095285a56ce5da7d6232f585f4d89b6b5bdf3e49
tugas <aldayanuar6@gmail.com>
2025-05-08 13:55:21 +0700

    percobaan 1

---
+++ P1Jobsheet10/Queue25.java
@@ -0,0 +1,97 @@
+package P1Jobsheet10;
+
+public class Queue25 {
+    int[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public Queue25(int n) {
+        max = n;
+        data = new int[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean IsEmpty() {
+        if (size == 0 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean IsFull() {
+        if(size==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void peek() {
+        if (!IsEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front]);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void print() {
+        if (IsEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i] + " ");
+                i = (i + 1) % max;
+            }
+            System.out.println(data[i] + " ");
+            System.out.println("Jumlah elemen = " + size);
+        }
+    }
+
+    public void clear() {
+        if (!IsEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void Enqueue(int dt) {
+        if (IsFull()) {
+            System.out.println("Queue sudah penuh");
+            System.exit(0);
+        } else {
+            if (IsEmpty()) {
+                front = rear = 0;
+            } else {
+                rear = (rear + 1) % max;
+            }
+            data[rear] = dt;
+            size++;
+        }
+    }
+
+    public int Dequeue() {
+        int dt = 0;
+        if (IsEmpty()) {
+            System.out.println("Queue masih kosong");
+            System.exit(0);
+        } else {
+            dt = data[front];
+            size--;
+            if (IsEmpty()) {
+                front = rear = -1;
+            } else {
+                front = (front + 1) % max;
+            }
+        }
+        return dt;
+    }
+}
+
